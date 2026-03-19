6b09ac646f329a4bf1667e6e206efc19d170ee00
Jahid Ansari <jahidansari7556@gmail.com>
2026-03-19 10:45:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ APIS = {
         'param': 'RC number',
         'log': LOG_CHANNELS['vehicle_to_number'],
         'desc': 'Vehicle Owner Mobile Number',
-        'extra_blacklist': []
+        'extra_blacklist': ['branding', 'credit']
     },
     'pincode': {
         'url': 'https://api.postalpincode.in/pincode/{}',
